9b7809a9b4a13ca547de6e650bb966ca6e46e8fb
Ben Noordhuis <info@bnoordhuis.nl>
2012-02-01 22:07:42 +0100

    buffers: honor length argument in base64 decoder
    Honor the length argument in `buf.write(s, 0, buf.length, 'base64')`. Before
    this commit, the length argument was ignored. The decoder would keep writing
    until it hit the end of the buffer. Since most buffers in Node are slices of
    a parent buffer (the slab), this bug would overwrite the content of adjacent
    buffers.

--- test/simple/test-buffer.js
+++ test/simple/test-buffer.js
@@ -687,7 +687,7 @@ assert.equal(Buffer._charsWritten, 9);
 buf.write('0123456789', 'binary');
 assert.equal(Buffer._charsWritten, 9);
 buf.write('123456', 'base64');
-assert.equal(Buffer._charsWritten, 6);
+assert.equal(Buffer._charsWritten, 4);
 buf.write('00010203040506070809', 'hex');
 assert.equal(Buffer._charsWritten, 18);
 
@@ -703,3 +703,8 @@ assert.equal(Buffer({length: 'BAM'}).length, 0);
 // Make sure that strings are not coerced to numbers.
 assert.equal(Buffer('99').length, 2);
 assert.equal(Buffer('13.37').length, 5);
+
+// Ensure that the length argument is respected.
+'ascii utf8 hex base64 binary'.split(' ').forEach(function(enc) {
+  assert.equal(Buffer(1).write('aaaaaa', 0, 1, enc), 1);
+});
