2d3cebeb3e33948b042384b1dd594f52bcf9c6f1
Ben Noordhuis <info@bnoordhuis.nl>
2013-02-04 17:03:43 +0100

    install: prepare for configurable libdir/mandir
    Lay the groundwork for configurable libdir and mandir directories.

--- tools/install.py
+++ tools/install.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 
-import errno
+# A target install path that is a directory MUST end with a slash ('/').
+# Omitting the slash will result in badly botched installs, to say nothing
+# of uninstalls.  You don't want to be the guy that ends up on Hacker News
+# because `sudo make uninstall` deleted someone's root file system.
 
 try:
   import json
 except ImportError:
   import simplejson as json
 
+import errno
 import os
 import re
 import shutil
 import sys
 
 # set at init time
-dst_dir = None
-node_prefix = None # dst_dir without DESTDIR prefix
+destdir = None
+node_prefix = None
 target_defaults = None
 variables = None
 
-def abspath(*args):
-  path = os.path.join(*args)
-  return os.path.abspath(path)
 
 def load_config():
   s = open('config.gypi').read()
@@ -28,59 +29,88 @@ def load_config():
   s = re.sub(r'\'', '"', s) # convert quotes
   return json.loads(s)
 
-def try_unlink(path):
+
+def abspath(*args):
+  return os.path.abspath(destdir + os.path.join(*args))
+
+
+def target_filename(source_path, prefix, target_path):
+  path = abspath(prefix, target_path)
+
+  if target_path.endswith('/'):
+    path = os.path.join(path,
+                        os.path.basename(source_path))
+
+  return path
+
+
+def try_unlink(source_path, prefix, target_path):
+  path = target_filename(source_path, prefix, target_path)
   try:
     os.unlink(path)
   except OSError, e:
     if e.errno != errno.ENOENT: raise
 
-def try_symlink(source_path, link_path):
-  print 'symlinking %s -> %s' % (source_path, link_path)
-  try_unlink(link_path)
-  os.symlink(source_path, link_path)
 
-def try_mkdir_r(path):
+def try_symlink(source_path, prefix, target_path):
+  path = abspath(prefix, target_path)
+  try_unlink(source_path, prefix, target_path)
+  os.symlink(source_path, path)
+
+
+def try_mkdir_r(prefix, target_path):
+  path = abspath(prefix, target_path)
   try:
     os.makedirs(path)
   except OSError, e:
     if e.errno != errno.EEXIST: raise
 
-def try_rmdir_r(path):
-  path = abspath(path)
-  while path.startswith(dst_dir):
+
+def try_rmdir_r(prefix, target_path):
+  path = abspath(prefix, target_path)
+  root = abspath('/')
+  while path > root:
     try:
       os.rmdir(path)
     except OSError, e:
       if e.errno == errno.ENOTEMPTY: return
       if e.errno == errno.ENOENT: return
       raise
-    path = abspath(path, '..')
+    # Move up to the parent directory.
+    path = os.path.join(path, '..')
+    path = os.path.abspath(path)
 
-def mkpaths(path, dst):
-  if dst.endswith('/'):
-    target_path = abspath(dst_dir, dst, os.path.basename(path))
-  else:
-    target_path = abspath(dst_dir, dst)
-  return path, target_path
 
-def try_copy(path, dst):
-  source_path, target_path = mkpaths(path, dst)
-  print 'installing %s' % target_path
-  try_mkdir_r(os.path.dirname(target_path))
-  try_unlink(target_path) # prevent ETXTBSY errors
-  return shutil.copy2(source_path, target_path)
+def try_copy(source_path, prefix, target_path):
+  path = abspath(prefix, target_path)
+  print 'installing %s' % target_filename(source_path, prefix, target_path)
+  try_mkdir_r(prefix,
+              os.path.dirname(target_path))
+  try_unlink(source_path, prefix, target_path)
+  shutil.copy2(source_path,
+               abspath(prefix, target_path))
 
-def try_remove(path, dst):
-  source_path, target_path = mkpaths(path, dst)
-  print 'removing %s' % target_path
-  try_unlink(target_path)
-  try_rmdir_r(os.path.dirname(target_path))
 
-def install(paths, dst): map(lambda path: try_copy(path, dst), paths)
-def uninstall(paths, dst): map(lambda path: try_remove(path, dst), paths)
+def try_remove(source_path, prefix, target_path):
+  path = abspath(prefix, target_path)
+  print 'removing %s from %s' % (source_path, target_path)
+  try_unlink(source_path, prefix, target_path)
+  try_rmdir_r(prefix,
+              os.path.dirname(target_path))
+
+
+def install(paths, prefix, target_path):
+  for path in paths:
+    try_copy(path, prefix, target_path)
+
+
+def uninstall(paths, prefix, target_path):
+  for path in paths:
+    try_remove(path, prefix, target_path)
+
 
 def waf_files(action):
-  action(['tools/node-waf'], 'bin/node-waf')
+  action(['tools/node-waf'], node_prefix, 'bin/node-waf')
   action(['tools/wafadmin/ansiterm.py',
           'tools/wafadmin/Build.py',
           'tools/wafadmin/Configure.py',
@@ -97,7 +127,7 @@ def waf_files(action):
           'tools/wafadmin/TaskGen.py',
           'tools/wafadmin/Task.py',
           'tools/wafadmin/Utils.py'],
-          'lib/node/wafadmin/')
+          node_prefix, 'lib/node/wafadmin/')
   action(['tools/wafadmin/Tools/ar.py',
           'tools/wafadmin/Tools/cc.py',
           'tools/wafadmin/Tools/ccroot.py',
@@ -131,34 +161,41 @@ def waf_files(action):
           'tools/wafadmin/Tools/winres.py',
           'tools/wafadmin/Tools/xlc.py',
           'tools/wafadmin/Tools/xlcxx.py'],
-          'lib/node/wafadmin/Tools/')
+          node_prefix, 'lib/node/wafadmin/Tools/')
 
-def update_shebang(path, shebang):
+
+def update_shebang(prefix, path, shebang):
+  path = abspath(prefix, path)
   print 'updating shebang of %s to %s' % (path, shebang)
   s = open(path, 'r').read()
   s = re.sub(r'#!.*\n', '#!' + shebang + '\n', s)
   open(path, 'w').write(s)
+
 
 def npm_files(action):
   target_path = 'lib/node_modules/npm/'
 
   # don't install npm if the target path is a symlink, it probably means
   # that a dev version of npm is installed there
-  if os.path.islink(abspath(dst_dir, target_path)): return
+  if os.path.islink(abspath(node_prefix, target_path)): return
 
   # npm has a *lot* of files and it'd be a pain to maintain a fixed list here
   # so we walk its source directory instead...
   for dirname, subdirs, basenames in os.walk('deps/npm', topdown=True):
     subdirs[:] = filter('test'.__ne__, subdirs) # skip test suites
     paths = [os.path.join(dirname, basename) for basename in basenames]
-    action(paths, target_path + dirname[9:] + '/')
+    action(paths,
+           node_prefix,
+           target_path + dirname[9:] + '/')
 
   # create/remove symlink
-  link_path = abspath(dst_dir, 'bin/npm')
+  link_path = 'bin/npm'
   if action == uninstall:
-    action([link_path], 'bin/npm')
+    action([link_path], node_prefix, 'bin/npm')
   elif action == install:
-    try_symlink('../lib/node_modules/npm/bin/npm-cli.js', link_path)
+    try_symlink('../lib/node_modules/npm/bin/npm-cli.js',
+                node_prefix,
+                link_path)
     if os.environ.get('PORTABLE'):
       # This crazy hack is necessary to make the shebang execute the copy
       # of node relative to the same directory as the npm script. The precompiled
@@ -168,9 +205,10 @@ def npm_files(action):
       shebang = '/bin/sh\n// 2>/dev/null; exec "`dirname "$0"`/node" "$0" "$@"'
     else:
       shebang = os.path.join(node_prefix, 'bin/node')
-    update_shebang(link_path, shebang)
+    update_shebang(node_prefix, link_path, shebang)
   else:
     assert(0) # unhandled action type
+
 
 def files(action):
   action(['deps/uv/include/ares.h',
@@ -188,47 +226,50 @@ def files(action):
           'src/node_buffer.h',
           'src/node_object_wrap.h',
           'src/node_version.h'],
-          'include/node/')
+          node_prefix, 'include/node/')
   action(['deps/uv/include/uv-private/eio.h',
           'deps/uv/include/uv-private/ev.h',
           'deps/uv/include/uv-private/ngx-queue.h',
           'deps/uv/include/uv-private/tree.h',
           'deps/uv/include/uv-private/uv-unix.h',
           'deps/uv/include/uv-private/uv-win.h'],
-          'include/node/uv-private/')
-  action(['out/Release/node'], 'bin/node')
+          node_prefix, 'include/node/uv-private/')
+  action(['out/Release/node'], node_prefix, 'bin/node')
 
   # install unconditionally, checking if the platform supports dtrace doesn't
   # work when cross-compiling and besides, there's at least one linux flavor
   # with dtrace support now (oracle's "unbreakable" linux)
-  action(['src/node.d'], 'lib/dtrace/')
+  action(['src/node.d'], node_prefix, 'lib/dtrace/')
 
   if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
-    action(['doc/node.1'], 'man/man1/')
+    action(['doc/node.1'], node_prefix, 'man/man1/')
   else:
-    action(['doc/node.1'], 'share/man/man1/')
+    action(['doc/node.1'], node_prefix, 'share/man/man1/')
 
   if 'true' == variables.get('node_install_waf'): waf_files(action)
   if 'true' == variables.get('node_install_npm'): npm_files(action)
 
+
 def run(args):
-  global dst_dir, node_prefix, target_defaults, variables
+  global destdir, node_prefix, target_defaults, variables
 
   # chdir to the project's top-level directory
-  os.chdir(abspath(os.path.dirname(__file__), '..'))
+  os.chdir(os.path.join(os.path.dirname(__file__), '..'))
 
   conf = load_config()
   variables = conf['variables']
   target_defaults = conf['target_defaults']
 
-  # argv[2] is a custom install prefix for packagers (think DESTDIR)
-  dst_dir = node_prefix = variables.get('node_prefix') or '/usr/local'
-  if len(args) > 2: dst_dir = abspath(args[2] + '/' + dst_dir)
+  node_prefix = variables.get('node_prefix') or '/usr/local'
 
-  cmd = args[1] if len(args) > 1 else 'install'
+  # argv[2] is a custom install prefix for packagers (think DESTDIR)
+  destdir = os.path.abspath(args[2]) if args[2:3] and args[2] else ''
+  cmd = args[1] if args[1:2] else 'install'
+
   if cmd == 'install': return files(install)
   if cmd == 'uninstall': return files(uninstall)
   raise RuntimeError('Bad command: %s\n' % cmd)
+
 
 if __name__ == '__main__':
   run(sys.argv[:])
