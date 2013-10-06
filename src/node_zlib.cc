bnoordhuis/node

commit c79d5163e530892c62b08d8b814b588220c26ec8

src: remove ObjectWrap dependency from core
Drop the ObjectWrap dependency in favor of an internal WeakObject class.

Let's us stop worrying about API and ABI compatibility when making
changes to the way node.js deals with weakly persistent handles
internally.

src/node_zlib.cc
@@ -24,6 +24,9 @@
 
 #include "env.h"
 #include "env-inl.h"
+#include "weak-object.h"
+#include "weak-object-inl.h"
+
 #include "v8.h"
 #include "zlib.h"
 
@@ -64,11 +67,11 @@ void InitZlib(v8::Handle<v8::Object> target);
 /**
  * Deflate/Inflate
  */
-class ZCtx : public ObjectWrap {
+class ZCtx : public WeakObject {
  public:
 
-  ZCtx(Environment* env, node_zlib_mode mode)
-      : ObjectWrap()
+  ZCtx(Environment* env, Local<Object> wrap, node_zlib_mode mode)
+      : WeakObject(env->isolate(), wrap)
       , chunk_size_(0)
       , dictionary_(NULL)
       , dictionary_len_(0)
@@ -118,7 +121,7 @@ class ZCtx : public ObjectWrap {
 
   static void Close(const FunctionCallbackInfo<Value>& args) {
     HandleScope scope(node_isolate);
-    ZCtx *ctx = ObjectWrap::Unwrap<ZCtx>(args.This());
+    ZCtx* ctx = WeakObject::Unwrap<ZCtx>(args.This());
     ctx->Close();
   }
 
@@ -128,13 +131,13 @@ class ZCtx : public ObjectWrap {
     HandleScope scope(node_isolate);
     assert(args.Length() == 7);
 
-    ZCtx *ctx = ObjectWrap::Unwrap<ZCtx>(args.This());
+    ZCtx* ctx = WeakObject::Unwrap<ZCtx>(args.This());
     assert(ctx->init_done_ && "write before init");
     assert(ctx->mode_ != NONE && "already finalized");
 
     assert(!ctx->write_in_progress_ && "write already in progress");
     ctx->write_in_progress_ = true;
-    ctx->Ref();
+    ctx->ClearWeak();
 
     assert(!args[0]->IsUndefined() && "must provide flush value");
 
@@ -194,7 +197,7 @@ class ZCtx : public ObjectWrap {
                   ZCtx::Process,
                   ZCtx::After);
 
-    args.GetReturnValue().Set(ctx->persistent());
+    args.GetReturnValue().Set(ctx->weak_object(node_isolate));
   }
 
 
@@ -284,11 +287,11 @@ class ZCtx : public ObjectWrap {
     ctx->write_in_progress_ = false;
 
     // call the write() cb
-    Local<Object> handle = ctx->handle(node_isolate);
+    Local<Object> handle = ctx->weak_object(node_isolate);
     Local<Value> args[2] = { avail_in, avail_out };
     MakeCallback(env, handle, env->callback_string(), ARRAY_SIZE(args), args);
 
-    ctx->Unref();
+    ctx->MakeWeak();
   }
 
   static void Error(ZCtx* ctx, const char* message) {
@@ -301,7 +304,7 @@ class ZCtx : public ObjectWrap {
       message = ctx->strm_.msg;
     }
 
-    Local<Object> handle = ctx->handle(node_isolate);
+    Local<Object> handle = ctx->weak_object(node_isolate);
     HandleScope scope(node_isolate);
     Local<Value> args[2] = {
       OneByteString(node_isolate, message),
@@ -311,7 +314,7 @@ class ZCtx : public ObjectWrap {
 
     // no hope of rescue.
     ctx->write_in_progress_ = false;
-    ctx->Unref();
+    ctx->MakeWeak();
   }
 
   static void New(const FunctionCallbackInfo<Value>& args) {
@@ -327,8 +330,7 @@ class ZCtx : public ObjectWrap {
       return ThrowTypeError("Bad argument");
     }
 
-    ZCtx* ctx = new ZCtx(env, mode);
-    ctx->Wrap(args.This());
+    new ZCtx(env, args.This(), mode);
   }
 
   // just pull the ints out of the args and call the other Init
@@ -338,7 +340,7 @@ class ZCtx : public ObjectWrap {
     assert((args.Length() == 4 || args.Length() == 5) &&
            "init(windowBits, level, memLevel, strategy, [dictionary])");
 
-    ZCtx *ctx = ObjectWrap::Unwrap<ZCtx>(args.This());
+    ZCtx* ctx = WeakObject::Unwrap<ZCtx>(args.This());
 
     int windowBits = args[0]->Uint32Value();
     assert((windowBits >= 8 && windowBits <= 15) && "invalid windowBits");
@@ -377,7 +379,7 @@ class ZCtx : public ObjectWrap {
 
     assert(args.Length() == 2 && "params(level, strategy)");
 
-    ZCtx* ctx = ObjectWrap::Unwrap<ZCtx>(args.This());
+    ZCtx* ctx = WeakObject::Unwrap<ZCtx>(args.This());
 
     Params(ctx, args[0]->Int32Value(), args[1]->Int32Value());
   }
@@ -385,7 +387,7 @@ class ZCtx : public ObjectWrap {
   static void Reset(const FunctionCallbackInfo<Value> &args) {
     HandleScope scope(node_isolate);
 
-    ZCtx *ctx = ObjectWrap::Unwrap<ZCtx>(args.This());
+    ZCtx* ctx = WeakObject::Unwrap<ZCtx>(args.This());
 
     Reset(ctx);
     SetDictionary(ctx);

node.gyp
@@ -144,6 +144,8 @@
         'src/tree.h',
         'src/util.h',
         'src/util-inl.h',
+        'src/weak-object.h',
+        'src/weak-object-inl.h',
         'deps/http_parser/http_parser.h',
         '<(SHARED_INTERMEDIATE_DIR)/node_natives.h',
         # javascript files to make for an even more pleasant IDE experience

src/node_contextify.cc
@@ -24,6 +24,8 @@
 #include "node_watchdog.h"
 #include "env.h"
 #include "env-inl.h"
+#include "weak-object.h"
+#include "weak-object-inl.h"
 
 namespace node {
 
@@ -304,7 +306,7 @@ class ContextifyContext {
   }
 };
 
-class ContextifyScript : public ObjectWrap {
+class ContextifyScript : public WeakObject {
  private:
   Persistent<Script> script_;
 
@@ -335,8 +337,8 @@ class ContextifyScript : public ObjectWrap {
       return ThrowError("Must call vm.Script as a constructor.");
     }
 
-    ContextifyScript *contextify_script = new ContextifyScript();
-    contextify_script->Wrap(args.Holder());
+    ContextifyScript* contextify_script =
+        new ContextifyScript(args.GetIsolate(), args.This());
 
     TryCatch try_catch;
     Local<String> code = args[0]->ToString();
@@ -497,7 +499,7 @@ class ContextifyScript : public ObjectWrap {
     }
 
     ContextifyScript* wrapped_script =
-        ObjectWrap::Unwrap<ContextifyScript>(args.This());
+        WeakObject::Unwrap<ContextifyScript>(args.This());
     Local<Script> script = PersistentToLocal(node_isolate,
                                              wrapped_script->script_);
 
@@ -524,6 +526,11 @@ class ContextifyScript : public ObjectWrap {
     }
 
     args.GetReturnValue().Set(result);
+  }
+
+
+  ContextifyScript(Isolate* isolate, Local<Object> object)
+      : WeakObject(isolate, object) {
   }
 
 

src/node_crypto.h
@@ -32,6 +32,9 @@
 #endif
 
 #include "env.h"
+#include "weak-object.h"
+#include "weak-object-inl.h"
+
 #include "v8.h"
 
 #include <openssl/ssl.h>
@@ -57,7 +60,7 @@ extern X509_STORE* root_cert_store;
 // Forward declaration
 class Connection;
 
-class SecureContext : public ObjectWrap {
+class SecureContext : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -90,8 +93,8 @@ class SecureContext : public ObjectWrap {
   static void GetTicketKeys(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetTicketKeys(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  explicit SecureContext(Environment* env)
-      : ObjectWrap()
+  SecureContext(Environment* env, v8::Local<v8::Object> wrap)
+      : WeakObject(env->isolate(), wrap)
       , ca_store_(NULL)
       , ctx_(NULL)
       , env_(env) {
@@ -219,7 +222,7 @@ class SSLWrap {
   friend class SecureContext;
 };
 
-class Connection : public SSLWrap<Connection>, public ObjectWrap {
+class Connection : public SSLWrap<Connection>, public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -273,15 +276,17 @@ class Connection : public SSLWrap<Connection>, public ObjectWrap {
   void SetShutdownFlags();
 
   static Connection* Unwrap(v8::Local<v8::Object> object) {
-    Connection* conn = ObjectWrap::Unwrap<Connection>(object);
+    Connection* conn = WeakObject::Unwrap<Connection>(object);
     conn->ClearError();
     return conn;
   }
 
   Connection(Environment* env,
+             v8::Local<v8::Object> wrap,
              SecureContext* sc,
              SSLWrap<Connection>::Kind kind)
       : SSLWrap<Connection>(env, sc, kind)
+      , WeakObject(env->isolate(), wrap)
       , bio_read_(NULL)
       , bio_write_(NULL)
       , hello_offset_(0) {
@@ -312,7 +317,7 @@ class Connection : public SSLWrap<Connection>, public ObjectWrap {
   friend class SecureContext;
 };
 
-class CipherBase : public ObjectWrap {
+class CipherBase : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -339,9 +344,13 @@ class CipherBase : public ObjectWrap {
   static void Final(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetAutoPadding(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  explicit CipherBase(CipherKind kind) : cipher_(NULL),
-                                         initialised_(false),
-                                         kind_(kind) {
+  CipherBase(v8::Isolate* isolate,
+             v8::Local<v8::Object> wrap,
+             CipherKind kind)
+      : WeakObject(isolate, wrap)
+      , cipher_(NULL)
+      , initialised_(false)
+      , kind_(kind) {
   }
 
   ~CipherBase() {
@@ -356,7 +365,7 @@ class CipherBase : public ObjectWrap {
   CipherKind kind_;
 };
 
-class Hmac : public ObjectWrap {
+class Hmac : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -370,7 +379,10 @@ class Hmac : public ObjectWrap {
   static void HmacUpdate(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void HmacDigest(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  Hmac() : md_(NULL), initialised_(false) {
+  Hmac(v8::Isolate* isolate, v8::Local<v8::Object> wrap)
+      : WeakObject(isolate, wrap)
+      , md_(NULL)
+      , initialised_(false) {
   }
 
   ~Hmac() {
@@ -384,7 +396,7 @@ class Hmac : public ObjectWrap {
   bool initialised_;
 };
 
-class Hash : public ObjectWrap {
+class Hash : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -396,7 +408,10 @@ class Hash : public ObjectWrap {
   static void HashUpdate(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void HashDigest(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  Hash() : md_(NULL), initialised_(false) {
+  Hash(v8::Isolate* isolate, v8::Local<v8::Object> wrap)
+      : WeakObject(isolate, wrap)
+      , md_(NULL)
+      , initialised_(false) {
   }
 
   ~Hash() {
@@ -410,7 +425,7 @@ class Hash : public ObjectWrap {
   bool initialised_;
 };
 
-class Sign : public ObjectWrap {
+class Sign : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -427,7 +442,10 @@ class Sign : public ObjectWrap {
   static void SignUpdate(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SignFinal(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  Sign() : md_(NULL), initialised_(false) {
+  Sign(v8::Isolate* isolate, v8::Local<v8::Object> wrap)
+      : WeakObject(isolate, wrap)
+      , md_(NULL)
+      , initialised_(false) {
   }
 
   ~Sign() {
@@ -441,7 +459,7 @@ class Sign : public ObjectWrap {
   bool initialised_;
 };
 
-class Verify : public ObjectWrap {
+class Verify : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -458,7 +476,10 @@ class Verify : public ObjectWrap {
   static void VerifyUpdate(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void VerifyFinal(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  Verify() : md_(NULL), initialised_(false) {
+  Verify(v8::Isolate* isolate, v8::Local<v8::Object> wrap)
+      : WeakObject(isolate, wrap)
+      , md_(NULL)
+      , initialised_(false) {
   }
 
   ~Verify() {
@@ -472,7 +493,7 @@ class Verify : public ObjectWrap {
   bool initialised_;
 };
 
-class DiffieHellman : public ObjectWrap {
+class DiffieHellman : public WeakObject {
  public:
   static void Initialize(Environment* env, v8::Handle<v8::Object> target);
 
@@ -493,7 +514,10 @@ class DiffieHellman : public ObjectWrap {
   static void SetPublicKey(const v8::FunctionCallbackInfo<v8::Value>& args);
   static void SetPrivateKey(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  DiffieHellman() : ObjectWrap(), initialised_(false), dh(NULL) {
+  DiffieHellman(v8::Isolate* isolate, v8::Local<v8::Object> wrap)
+      : WeakObject(isolate, wrap)
+      , initialised_(false)
+      , dh(NULL) {
   }
 
   ~DiffieHellman() {

src/node_http_parser.cc
@@ -25,6 +25,8 @@
 
 #include "env.h"
 #include "env-inl.h"
+#include "weak-object.h"
+#include "weak-object-inl.h"
 #include "v8.h"
 
 #include <stdlib.h>  // free()
@@ -185,10 +187,10 @@ struct StringPtr {
 };
 
 
-class Parser : public ObjectWrap {
+class Parser : public WeakObject {
  public:
-  Parser(Environment* env, enum http_parser_type type)
-      : ObjectWrap()
+  Parser(Environment* env, Local<Object> wrap, enum http_parser_type type)
+      : WeakObject(env->isolate(), wrap)
       , env_(env)
       , current_buffer_len_(0)
       , current_buffer_data_(NULL) {
@@ -252,7 +254,7 @@ class Parser : public ObjectWrap {
 
 
   HTTP_CB(on_headers_complete) {
-    Local<Object> obj = handle(node_isolate);
+    Local<Object> obj = weak_object(node_isolate);
     Local<Value> cb = obj->Get(kOnHeadersComplete);
 
     if (!cb->IsFunction())
@@ -313,7 +315,7 @@ class Parser : public ObjectWrap {
   HTTP_DATA_CB(on_body) {
     HandleScope scope(node_isolate);
 
-    Local<Object> obj = handle(node_isolate);
+    Local<Object> obj = weak_object(node_isolate);
     Local<Value> cb = obj->Get(kOnBody);
 
     if (!cb->IsFunction())
@@ -342,7 +344,7 @@ class Parser : public ObjectWrap {
     if (num_fields_)
       Flush();  // Flush trailing HTTP headers.
 
-    Local<Object> obj = handle(node_isolate);
+    Local<Object> obj = weak_object(node_isolate);
     Local<Value> cb = obj->Get(kOnMessageComplete);
 
     if (!cb->IsFunction())
@@ -362,13 +364,10 @@ class Parser : public ObjectWrap {
   static void New(const FunctionCallbackInfo<Value>& args) {
     Environment* env = Environment::GetCurrent(args.GetIsolate());
     HandleScope handle_scope(args.GetIsolate());
-
     http_parser_type type =
         static_cast<http_parser_type>(args[0]->Int32Value());
-
     assert(type == HTTP_REQUEST || type == HTTP_RESPONSE);
-    Parser* parser = new Parser(env, type);
-    parser->Wrap(args.This());
+    new Parser(env, args.This(), type);
   }
 
 
@@ -389,7 +388,7 @@ class Parser : public ObjectWrap {
   static void Execute(const FunctionCallbackInfo<Value>& args) {
     HandleScope scope(node_isolate);
 
-    Parser* parser = ObjectWrap::Unwrap<Parser>(args.This());
+    Parser* parser = WeakObject::Unwrap<Parser>(args.This());
     assert(parser->current_buffer_.IsEmpty());
     assert(parser->current_buffer_len_ == 0);
     assert(parser->current_buffer_data_ == NULL);
@@ -443,7 +442,7 @@ class Parser : public ObjectWrap {
   static void Finish(const FunctionCallbackInfo<Value>& args) {
     HandleScope scope(node_isolate);
 
-    Parser* parser = ObjectWrap::Unwrap<Parser>(args.This());
+    Parser* parser = WeakObject::Unwrap<Parser>(args.This());
 
     assert(parser->current_buffer_.IsEmpty());
     parser->got_exception_ = false;
@@ -476,7 +475,7 @@ class Parser : public ObjectWrap {
         static_cast<http_parser_type>(args[0]->Int32Value());
 
     assert(type == HTTP_REQUEST || type == HTTP_RESPONSE);
-    Parser* parser = ObjectWrap::Unwrap<Parser>(args.This());
+    Parser* parser = WeakObject::Unwrap<Parser>(args.This());
     // Should always be called from the same context.
     assert(env == parser->env());
     parser->Init(type);
@@ -503,7 +502,7 @@ class Parser : public ObjectWrap {
   void Flush() {
     HandleScope scope(node_isolate);
 
-    Local<Object> obj = handle(node_isolate);
+    Local<Object> obj = weak_object(node_isolate);
     Local<Value> cb = obj->Get(kOnHeaders);
 
     if (!cb->IsFunction())

src/node_stat_watcher.cc
@@ -22,6 +22,8 @@
 #include "node_stat_watcher.h"
 #include "env.h"
 #include "env-inl.h"
+#include "weak-object.h"
+#include "weak-object-inl.h"
 
 #include <assert.h>
 #include <string.h>
@@ -61,8 +63,8 @@ static void Delete(uv_handle_t* handle) {
 }
 
 
-StatWatcher::StatWatcher(Environment* env)
-    : ObjectWrap()
+StatWatcher::StatWatcher(Environment* env, Local<Object> wrap)
+    : WeakObject(env->isolate(), wrap)
     , watcher_(new uv_fs_poll_t)
     , env_(env) {
   uv_fs_poll_init(env->event_loop(), watcher_);
@@ -91,7 +93,7 @@ void StatWatcher::Callback(uv_fs_poll_t* handle,
     Integer::New(status, node_isolate)
   };
   MakeCallback(env,
-               wrap->handle(node_isolate),
+               wrap->weak_object(node_isolate),
                env->onchange_string(),
                ARRAY_SIZE(argv),
                argv);
@@ -102,8 +104,7 @@ void StatWatcher::New(const FunctionCallbackInfo<Value>& args) {
   assert(args.IsConstructCall());
   Environment* env = Environment::GetCurrent(args.GetIsolate());
   HandleScope handle_scope(args.GetIsolate());
-  StatWatcher* s = new StatWatcher(env);
-  s->Wrap(args.This());
+  new StatWatcher(env, args.This());
 }
 
 
@@ -111,23 +112,23 @@ void StatWatcher::Start(const FunctionCallbackInfo<Value>& args) {
   assert(args.Length() == 3);
   HandleScope scope(node_isolate);
 
-  StatWatcher* wrap = ObjectWrap::Unwrap<StatWatcher>(args.This());
+  StatWatcher* wrap = WeakObject::Unwrap<StatWatcher>(args.This());
   String::Utf8Value path(args[0]);
   const bool persistent = args[1]->BooleanValue();
   const uint32_t interval = args[2]->Uint32Value();
 
   if (!persistent) uv_unref(reinterpret_cast<uv_handle_t*>(wrap->watcher_));
   uv_fs_poll_start(wrap->watcher_, Callback, *path, interval);
-  wrap->Ref();
+  wrap->ClearWeak();
 }
 
 
 void StatWatcher::Stop(const FunctionCallbackInfo<Value>& args) {
-  StatWatcher* wrap = ObjectWrap::Unwrap<StatWatcher>(args.This());
+  StatWatcher* wrap = WeakObject::Unwrap<StatWatcher>(args.This());
   Environment* env = wrap->env();
   Context::Scope context_scope(env->context());
   HandleScope handle_scope(env->isolate());
-  MakeCallback(env, wrap->handle(node_isolate), env->onstop_string());
+  MakeCallback(env, wrap->weak_object(node_isolate), env->onstop_string());
   wrap->Stop();
 }
 
@@ -135,7 +136,7 @@ void StatWatcher::Stop(const FunctionCallbackInfo<Value>& args) {
 void StatWatcher::Stop() {
   if (!uv_is_active(reinterpret_cast<uv_handle_t*>(watcher_))) return;
   uv_fs_poll_stop(watcher_);
-  Unref();
+  MakeWeak();
 }
 
 

src/tls_wrap.cc
@@ -58,7 +58,7 @@ TLSCallbacks::TLSCallbacks(Environment* env,
                            Kind kind,
                            Handle<Object> sc,
                            StreamWrapCallbacks* old)
-    : SSLWrap<TLSCallbacks>(env, ObjectWrap::Unwrap<SecureContext>(sc), kind),
+    : SSLWrap<TLSCallbacks>(env, WeakObject::Unwrap<SecureContext>(sc), kind),
       StreamWrapCallbacks(old),
       enc_in_(NULL),
       enc_out_(NULL),
@@ -70,7 +70,7 @@ TLSCallbacks::TLSCallbacks(Environment* env,
       shutdown_(false) {
 
   // Persist SecureContext
-  sc_ = ObjectWrap::Unwrap<SecureContext>(sc);
+  sc_ = WeakObject::Unwrap<SecureContext>(sc);
   sc_handle_.Reset(node_isolate, sc);
 
   Local<Object> object = env->tls_wrap_constructor_function()->NewInstance();
@@ -236,7 +236,7 @@ void TLSCallbacks::SSLInfoCallback(const SSL* ssl_, int where, int ret) {
   // There should be a Context::Scope a few stack frames down.
   assert(env->context() == env->isolate()->GetCurrentContext());
   HandleScope handle_scope(env->isolate());
-  Local<Object> object = c->object(env->isolate());
+  Local<Object> object = c->weak_object(env->isolate());
 
   if (where & SSL_CB_HANDSHAKE_START) {
     Local<Value> callback = object->Get(env->onhandshakestart_string());
@@ -313,7 +313,7 @@ void TLSCallbacks::EncOutCb(uv_write_t* req, int status) {
         FIXED_ONE_BYTE_STRING(node_isolate, "write cb error, status: "),
         Integer::New(status, node_isolate)->ToString());
     MakeCallback(env,
-                 callbacks->object(node_isolate),
+                 callbacks->weak_object(node_isolate),
                  env->onerror_string(),
                  1,
                  &arg);
@@ -398,7 +398,7 @@ void TLSCallbacks::ClearOut() {
 
     if (!argv.IsEmpty()) {
       MakeCallback(env(),
-                   object(node_isolate),
+                   weak_object(node_isolate),
                    env()->onerror_string(),
                    1,
                    &argv);
@@ -437,7 +437,7 @@ bool TLSCallbacks::ClearIn() {
   Handle<Value> argv = GetSSLError(written, &err);
   if (!argv.IsEmpty()) {
     MakeCallback(env(),
-                 object(node_isolate),
+                 weak_object(node_isolate),
                  env()->onerror_string(),
                  1,
                  &argv);
@@ -504,7 +504,7 @@ int TLSCallbacks::DoWrite(WriteWrap* w,
     Handle<Value> argv = GetSSLError(written, &err);
     if (!argv.IsEmpty()) {
       MakeCallback(env(),
-                   object(node_isolate),
+                   weak_object(node_isolate),
                    env()->onerror_string(),
                    1,
                    &argv);
@@ -688,7 +688,7 @@ int TLSCallbacks::SelectSNIContextCallback(SSL* s, int* ad, void* arg) {
 
   if (servername != NULL) {
     // Call the SNI callback and use its return value as context
-    Local<Object> object = p->object(node_isolate);
+    Local<Object> object = p->weak_object(node_isolate);
     Local<Value> ctx = object->Get(env->sni_context_string());
 
     if (!ctx->IsObject())
@@ -697,7 +697,7 @@ int TLSCallbacks::SelectSNIContextCallback(SSL* s, int* ad, void* arg) {
     p->sni_context_.Dispose();
     p->sni_context_.Reset(node_isolate, ctx);
 
-    SecureContext* sc = ObjectWrap::Unwrap<SecureContext>(ctx.As<Object>());
+    SecureContext* sc = WeakObject::Unwrap<SecureContext>(ctx.As<Object>());
     SSL_set_SSL_CTX(s, sc->ctx_);
   }
 

commit c66e23f10e61d039840b9cde41fdb182c29d32fe

zlib: fix write request reference counting
Keep track of the reference count, don't make the wrapper object weak
when there are pending write requests.  Fixes a regression from c79d516.

src/node_zlib.cc
@@ -84,7 +84,8 @@ class ZCtx : public WeakObject {
       , mode_(mode)
       , strategy_(0)
       , windowBits_(0)
-      , write_in_progress_(false) {
+      , write_in_progress_(false)
+      , refs_(0) {
   }
 
 
@@ -137,7 +138,7 @@ class ZCtx : public WeakObject {
 
     assert(!ctx->write_in_progress_ && "write already in progress");
     ctx->write_in_progress_ = true;
-    ctx->ClearWeak();
+    ctx->Ref();
 
     assert(!args[0]->IsUndefined() && "must provide flush value");
 
@@ -291,7 +292,7 @@ class ZCtx : public WeakObject {
     Local<Value> args[2] = { avail_in, avail_out };
     MakeCallback(env, handle, env->callback_string(), ARRAY_SIZE(args), args);
 
-    ctx->MakeWeak();
+    ctx->Unref();
   }
 
   static void Error(ZCtx* ctx, const char* message) {
@@ -314,7 +315,7 @@ class ZCtx : public WeakObject {
 
     // no hope of rescue.
     ctx->write_in_progress_ = false;
-    ctx->MakeWeak();
+    ctx->Unref();
   }
 
   static void New(const FunctionCallbackInfo<Value>& args) {
@@ -517,6 +518,19 @@ class ZCtx : public WeakObject {
   }
 
  private:
+  void Ref() {
+    if (++refs_ == 1) {
+      ClearWeak();
+    }
+  }
+
+  void Unref() {
+    assert(refs_ > 0);
+    if (--refs_ == 0) {
+      MakeWeak();
+    }
+  }
+
   static const int kDeflateContextSize = 16384;  // approximate
   static const int kInflateContextSize = 10240;  // approximate
 
@@ -535,6 +549,7 @@ class ZCtx : public WeakObject {
   int windowBits_;
   uv_work_t work_req_;
   bool write_in_progress_;
+  unsigned int refs_;
 };
 
 